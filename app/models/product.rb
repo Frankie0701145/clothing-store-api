992777155d5ea55e4ae4f3f37888d78b5a578da9
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-20 08:17:11 +0300

    Configure annotate.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
-#product_type: string: index
-
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :integer          not null, primary key
+#  product_name :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_products_on_product_name  (product_name)
+#
 class Product < ApplicationRecord
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :integer          not null, primary key
+#  product_name :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_products_on_product_name  (product_name)
+#
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
