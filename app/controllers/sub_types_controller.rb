e117c50aec14c7a42a8f83463c2ea10425f2b991
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-20 20:38:13 +0300

    Create the sub_type controller.

---
+++ app/controllers/sub_types_controller.rb
@@ -0,0 +1,24 @@
+class SubTypesController < ApplicationController
+
+    def create
+        #add sub_types to a product
+        product = Product.find_by_id params[:product_id]
+        if product.nil?
+            #if there is no product throw error
+            raise ActiveRecord::RecordNotFound
+        else
+            sub_types = product.subtypes.create sub_types_params
+            render json: {data: {sub_types: sub_types}}, status: :ok
+        end
+    end
+
+    def show
+        sub_types = SubType.where product_id: param[:product_id]
+        render json: {data: sub_types}, status: :ok
+    end
+
+    private
+        def sub_types_params
+            params.permit( sub_types:[])
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace "api" do
-    post "/products", to: "products#create"
+    post "/products", to: "products#create", as: "create_product"
+    patch "/products/:id", to: "prdoducts#add_sub_types", as: "add_sub_types_to_product"
+    post "/sub_types/:product_id", to: "sub_types#create"
+    get "/sub_types/:product_id", to: "sub_types#show"
   end
 end
