aa731784bae5bda2870029d1a00866614808deb3
Francis Njuguna <coulsorfrancois@gmail.com>
2020-06-21 08:32:47 +0300

    Add params permit scala to the types_params.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,13 @@
 class ProductsController < ApplicationController
 
     def create
-     
-        #find_or_create
-        p products_sub_types_params
+        #instantiate a product
         product = Product.new products_sub_types_params[:product]
-        
+        #build a sub_types
         sub_types = product.sub_types.build products_sub_types_params[:sub_types]
-
+        #save
         if product.save
+            #convert to json
             product_json = product.to_json(:include=>[:sub_types])     
             render json: product_json  
         else        
@@ -17,7 +16,7 @@ class ProductsController < ApplicationController
     end
 
     def show
-        #retrieve products with types and subtypes
+       
         products = Product.includes(types: :sub_types).all;
         products_json = products.to_json(:include=>{:types=>
                                                         {:include=> :sub_types}
@@ -29,16 +28,17 @@ class ProductsController < ApplicationController
         product = Product.find_by_id params[:product_id]
         if product.nil?
             render json: {errors: ["There is no product with that product_id"]}, status: :not_found
-        else
+        else 
             type = product.types.new({quantity: types_params[:quantity], price: types_params[:price]});
-            type.sub_type_ids = types_params[:sub_types_ids]
+            type.sub_type_ids = types_params[:sub_type_ids]
             if product.save
-                product_json = product.to_json(:include=>{:types=>
-                                                                 {:include=> :sub_types}
-                                                             })
-                render json: product_json, status: :ok
+                # type_json = type.to_json(:include=>{:types=>
+                #                                                     {:include=> :sub_types}
+                #                                                 })
+                type_json = type.to_json(:include=>:sub_types)
+                render json: type_json, status: :ok
             else
-                render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+                render json: {errors: type.errors.full_messages}, status: :unprocessable_entity
             end
         end
     end
@@ -46,16 +46,10 @@ class ProductsController < ApplicationController
     private
 
         def products_sub_types_params
-            # params.permit(sub_types:[{name: :String, value: :String}], product:{})
             params.permit!
         end
 
         def types_params
-            # params.permit(sub_types_ids:[], quantity: :Numeric , price: :Numeric )
-            # params.permit([:quantity, :price, sub_types_ids: []])
-            params.permit!
-            # subtypesIds: []
-            # quantity: integer
-            #price: integer
+            params.require(:type).permit(:quantity, :price, :product_id, sub_type_ids:[])
         end
 end
